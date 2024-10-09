fc64970ee5290c5c8777dc18dc70d026b0f57c43
Sourabh Jagtap <sourabhjagtap2344@gmail.com>
2024-10-09 22:29:44 +0530

    Setup basic nodyparser

--- src/config/serverconfig.js
+++ src/config/serverconfig.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+//exporting port value from enviranment variable
 module.exports = {
     PORT : process.env.PORT
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
-//fetch the port value from the provided location
+const bodyParser = require("body-parser");
+//fetch the port value from the provided location 
 const {PORT} = require('./config/serverconfig');
 const setupAndStartServer = async() => {
     //create express object
     const app = express();
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+
     app.listen(PORT, ()=>{
         console.log(`Server started at ${PORT}` )
     });
