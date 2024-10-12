6362d976a5d130ed4c64cbd9a5a56ffbbc1169dc
Sourabh Jagtap <sourabhjagtap2344@gmail.com>
2024-10-12 19:14:22 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -2,11 +2,12 @@ const {City} = require('../models/index')
 
 class CityRepository{
     //Create a city
-    async createCity({name}){
+    async createCity({ name }){
         try{
             const city = await City.create({name});
             return city;
         }catch(error){
+            console.log("Something went wrong in the repository layer ");
             throw {error};
         }
     }
@@ -20,6 +21,31 @@ class CityRepository{
                 }
             })
         }catch(error){
+            console.log("Something went wrong in the repository layer ");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId , data) {
+        try{
+            const city = await City.update(data , {
+                where : {
+                    id : cityId
+                }
+            });
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer ");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);//find by primary key
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer ");
             throw {error};
         }
     }
