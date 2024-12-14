e1589090a3650eaab428624fafcc54957aa5e620
Sourabh Jagtap <sourabhjagtap2344@gmail.com>
2024-12-14 22:44:02 +0530

    Added city controller

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,10 +1,16 @@
+/*
+when req comes to the controller it will pass all the data to the service 
+and repository layer and response from this layer give it back to controller 
+then controller forms a respomse structure and give ot back to the user
+*/
+
 const { CityServices } = require('../services/index');
 
 const cityService = new CityServices();
 
-const create = async (req, res) => {
+const create = async (req, res) => {//post request
     try {
-        const city = await cityService.createCity(req.body);
+        const city = await cityService.createCity(req.body);//user data is in request.body
         return res.status(201).json({
             data: city,
             success: true,
@@ -22,6 +28,72 @@ const create = async (req, res) => {
     }
 };
 
+//DELETE request  (URL -> /city/:id)
+const destroy = async (req, res) => {
+    try{
+        const response = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: 'Successfully deleted a city',
+            error: {}
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to delete the city',
+            error: error
+        });
+    }
+};
+
+//GET request  (URL -> /city/:id)
+const get = async (req, res) => {
+    try{
+        const response = await cityService.getCity(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: 'Successfully fetched a city',
+            error: {}
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to get the city',
+            error: error
+        });
+    }
+};
+
+//PATCH   (URL -> /city/:id [id will represent whivh city want to update] , request body will hold the data want to update ) 
+const update = async (req, res) => {
+    try{
+        const response = await cityService.updateCity(req.params.id,req.body);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: 'Successfully fetched a city',
+            error: {}
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to update the city',
+            error: error
+        });
+    }
+};
+
 module.exports = {
-    create
+    create,
+    destroy,
+    get,
+    update
 };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,5 +5,8 @@ const CityController = require('../../controllers/city-controller');
 const router = express.Router();
 
 router.post('/city' , CityController.create);
-
+router.delete('/city/:id',CityController.destroy);
+//here 'city/:id' is written bcz our city-controller.js contain 'req.params.id' in which "id" is wirtten in destroy so both must be same i.e id == id do not change the name
+//router.get('city/:id' , CityController.get);//need to write the function
+//router.patch('city/:id',CityController.update);//need to write the function
 module.exports = router;
