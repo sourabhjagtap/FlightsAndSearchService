d1b70e36b7fa60c28ff6a96643614fc2bccede19
Sourabh Jagtap <sourabhjagtap2344@gmail.com>
2024-12-14 11:42:27 +0530

    Added routers to complete create api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,101 +1,27 @@
-const { CityService } = require('../services/index');
+const { CityServices } = require('../services/index');
 
-//create an object of class CityService
-const cityService = new CityService();
+const cityService = new CityServices();
 
-/**
- * POST
- * data -> req.body
- */
-const create = async (req , res) => {
-    try{
+const create = async (req, res) => {
+    try {
         const city = await cityService.createCity(req.body);
         return res.status(201).json({
             data: city,
             success: true,
             message: 'Successfully created a city',
-            err: {}
+            error: {}
         });
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.status(500).json({
-            data:{},
+            data: {},
             success: false,
-            message: "Not able to create a city",
-            err: error
+            message: 'Not able to create a city',
+            error: error
         });
     }
-}
-
-// DELETE, -> /city/:id
-
-const destroy = async (req , res) => {
-    try{
-        const response = await cityService.deleteCity(req.params.id);
-        return res.status(200).json({
-            data: response,
-            success: true,
-            message: 'Successfully deleted a city',
-            err: {}
-        });
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({
-            data:{},
-            success: false,
-            message: "Not able to delete the city",
-            err: error
-        });
-    }
-}
-
-
-// GET -> /city/:id
-const get = async (req , res) => {
-    try{
-        const response = await cityService.getCity(req.params.id);
-        return res.status(200).json({
-            data: response,
-            success: true,
-            message: 'Successfully fetched a city',
-            err: {}
-        });
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({
-            data:{},
-            success: false,
-            message: "Not able to get the city",
-            err: error
-        });
-    }
-}
-
-
-// Patch( type of request ) -> /city/:id (hold the id of which city to update) -> req.body(hold the data of what data to update)
-const update = async (req , res) => {
-    try{
-        const response = await cityService.updateCity(req.params.id, req.body);
-        return res.status(200).json({
-            data: response,
-            success: true,
-            message: 'Successfully updated a city',
-            err: {}
-        });
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({
-            data:{},
-            success: false,
-            message: "Not able to update the city",
-            err: error
-        });
-    }
-}
+};
 
 module.exports = {
-    create,
-    destroy,
-    get,
-    update
-}
+    create
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 //fetch the port value from the provided location 
-const {PORT} = require('./config/serverconfig');
+const { PORT }  = require('./config/serverConfig');
 
-const cityRepository = require('./repository/city-repository');
-const CityRepository = require("./repository/city-repository");
+//get access to the routes object from ./routes/index.js
+const ApiRoutes = require('./routes/index')
+
+//const cityRepository = require('./repository/city-repository');
+//const CityRepository = require("./repository/city-repository");
 
 const setupAndStartServer = async() => {
     //create express object
@@ -13,12 +16,14 @@ const setupAndStartServer = async() => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    //so in any api request if we found /api then map it to ApiRoutes
+    app.use('/api',ApiRoutes);
 
-    app.listen(PORT, ()=>{
+    app.listen(PORT, async ()=>{
         console.log(`Server started at ${PORT}` )
-        const repo = new CityRepository();//object creation
-        repo.createCity({name : "New Delhi"});//creating new city 
+        //const repo = new CityRepository();//object creation
+        //repo.createCity({name : "New Delhi"});//creating new city 
     });
 }
 
-setupAndStartServer();
+setupAndStartServer(); 
