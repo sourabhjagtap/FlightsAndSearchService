bdbd532fd2be479baeba7635a4009e65a63e4051
Sourabh Jagtap <sourabhjagtap2344@gmail.com>
2024-12-17 00:19:35 +0530

    Added airport seeders

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ const { PORT }  = require('./config/serverConfig');
 //get access to the routes object from ./routes/index.js
 const ApiRoutes = require('./routes/index')
 
-//const cityRepository = require('./repository/city-repository');
-//const CityRepository = require("./repository/city-repository");
+const db = require('./models/index');
+//const sequelize = require('sequelize');
+const {City, Airport} = require('./models/index');
+const { where } = require("sequelize");
+
 
 const setupAndStartServer = async() => {
     //create express object
@@ -21,8 +24,20 @@ const setupAndStartServer = async() => {
 
     app.listen(PORT, async ()=>{
         console.log(`Server started at ${PORT}` )
-        //const repo = new CityRepository();//object creation
-        //repo.createCity({name : "New Delhi"});//creating new city 
+        if(process.env.SYNC_DB){//do sync one time 
+            db.sequelize.sync({alter : true});
+        }
+        //db.sequelize.sync({alter : true});//sync once only hence comment
+        const city = await City.findOne({//find city with id = 52
+            where:{
+                id:52
+            }
+        });
+        const airports = await city.getAirports();//find all the airports in city with id = 52 by its name (it will perform join operation)
+        console.log(city, airports);
+        // console.log(city);
+        
+
     });
 }
 
